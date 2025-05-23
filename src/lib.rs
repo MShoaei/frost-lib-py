378369f2a572d5fa2c05d99510e266e7af01a83a
sadegh <sadeghte@gmail.com>
2025-05-23 23:17:45 +0330

    add secp256k1

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,17 @@
 use pyo3::prelude::*;
 
 pub mod ed25519;
+pub mod secp256k1;
 pub mod secp256k1_tr;
 
 #[pymodule]
 fn frost_lib(py: Python, m: &PyModule) -> PyResult<()> {
     let secp_mod = PyModule::new(py, "ed25519")?;
     ed25519::register(py, secp_mod)?;
+    m.add_submodule(secp_mod)?;
+
+    let secp_mod = PyModule::new(py, "secp256k1")?;
+    secp256k1::register(py, secp_mod)?;
     m.add_submodule(secp_mod)?;
 
     let tr_mod = PyModule::new(py, "secp256k1_tr")?;
